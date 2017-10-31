49e2edda33d10dbecaa2e1e28b307ec0c799ec5a
Taejoon Kwon <tkwon@unist.ac.kr>
2017-10-31 11:55:08 +0900

    Compatible to python3

--- denovo/fastq-to-paired.py
+++ denovo/fastq-to-paired.py
@@ -1,22 +1,27 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import os
 import sys
 import gzip
 
+mesg_usage = '[Usage] fastq-to-paired.py '
+mesg_usage += '<R1.fq> <R2.fq> <output name> <read_len>'
+
+if len(sys.argv) != 5:
+    sys.stderr.write(mesg_usage+'\n')
+    sys.exit(1)
+
 filename_fq1 = sys.argv[1]
 filename_fq2 = sys.argv[2]
 filename_out = sys.argv[3]
+read_len = int(sys.argv[4])
 
 f_fq1 = open(filename_fq1,'r')
-if( filename_fq1.endswith('.gz') ):
-    f_fq1 = gzip.open(filename_fq1,'rb')
+if filename_fq1.endswith('.gz'):
+    f_fq1 = gzip.open(filename_fq1,'rt')
 
 f_fq2 = open(filename_fq2,'r')
-if( filename_fq2.endswith('.gz') ):
-    f_fq2 = gzip.open(filename_fq2,'rb')
-
-read1_len = 0
-read2_len = 0
+if filename_fq2.endswith('.gz'):
+    f_fq2 = gzip.open(filename_fq2,'rt')
 
 nfreq1_raw = dict()
 nfreq2_raw = dict()
@@ -28,34 +33,33 @@ count_nocall_1 = 0
 count_nocall_2 = 0
 count_nocall_12 = 0
 
+for tmp_i in range(0,read_len):
+    nfreq1_raw[tmp_i] = {'A':0,'T':0,'G':0,'C':0,'N':0}
+    nfreq1_called[tmp_i] = {'A':0,'T':0,'G':0,'C':0,'N':0}
+    nfreq2_raw[tmp_i] = {'A':0,'T':0,'G':0,'C':0,'N':0}
+    nfreq2_called[tmp_i] = {'A':0,'T':0,'G':0,'C':0,'N':0}
+
 f_out = open('%s_paired.fastq'%filename_out,'w')
 for line in f_fq1:
     h_nseq1 = line.strip()
-    nseq1 = f_fq1.next().strip()
-    h_qseq1 = f_fq1.next().strip()
-    qseq1 = f_fq1.next().strip()
+    nseq1 = f_fq1.readline().strip()
+    h_qseq1 = f_fq1.readline().strip()
+    qseq1 = f_fq1.readline().strip()
+    read1_len = len(nseq1)
 
-    if( read1_len == 0 ):
-        read1_len = len(nseq1)
-        for tmp_i in range(0,read1_len):
-            nfreq1_raw[tmp_i] = {'A':0,'T':0,'G':0,'C':0,'N':0}
-            nfreq1_called[tmp_i] = {'A':0,'T':0,'G':0,'C':0,'N':0}
-    
-    h_nseq2 = f_fq2.next().strip()
-    nseq2 = f_fq2.next().strip()
-    h_qseq2 = f_fq2.next().strip()
-    qseq2 = f_fq2.next().strip()
-    if( read2_len == 0 ):
-        read2_len = len(nseq2)
-        for tmp_i in range(0,read2_len):
-            nfreq2_raw[tmp_i] = {'A':0,'T':0,'G':0,'C':0,'N':0}
-            nfreq2_called[tmp_i] = {'A':0,'T':0,'G':0,'C':0,'N':0}
+    h_nseq2 = f_fq2.readline().strip()
+    nseq2 = f_fq2.readline().strip()
+    h_qseq2 = f_fq2.readline().strip()
+    qseq2 = f_fq2.readline().strip()
+    read2_len = len(nseq2)
     
     count_total += 1
     
     count_N1 = nseq1.count('N') + nseq1.count('.')
     count_N2 = nseq2.count('N') + nseq2.count('.')
     
+    #count_N1types = len(set(nseq1))
+    #count_N2types = len(set(nseq2))
     count_A1 = nseq1.count('A')
     count_A2 = nseq2.count('A')
     count_T1 = nseq1.count('T')
@@ -65,39 +69,29 @@ for line in f_fq1:
     count_C1 = nseq1.count('C')
     count_C2 = nseq2.count('C')
 
+    for tmp_i in range(0,read1_len):
+        nfreq1_raw[tmp_i][ nseq1[tmp_i] ] += 1
+    for tmp_i in range(0,read2_len):
+        nfreq2_raw[tmp_i][ nseq2[tmp_i] ] += 1
+
     if( count_N1 > 0 and count_N2 > 0 ):
         count_nocall_12 += 1
-        for tmp_i in range(0,read1_len):
-            nfreq1_raw[tmp_i][ nseq1[tmp_i] ] += 1
-        for tmp_i in range(0,read2_len):
-            nfreq2_raw[tmp_i][ nseq2[tmp_i] ] += 1
     elif( count_N1 > 0 or count_A1*count_T1*count_C1*count_G1 == 0 ):
         count_nocall_1 += 1
-        for tmp_i in range(0,read1_len):
-            nfreq1_raw[tmp_i][ nseq1[tmp_i] ] += 1
-
-        for tmp_i in range(0,read2_len):
-            nfreq2_raw[tmp_i][ nseq2[tmp_i] ] += 1
     elif( count_N2 > 0 or count_A2*count_T2*count_C2*count_G2 == 0 ):
         count_nocall_2 += 1
-        for tmp_i in range(0,read1_len):
-            nfreq1_raw[tmp_i][ nseq1[tmp_i] ] += 1
-        for tmp_i in range(0,read2_len):
-            nfreq2_raw[tmp_i][ nseq2[tmp_i] ] += 1
     else:
         for tmp_i in range(0,read1_len):
-            nfreq1_raw[tmp_i][ nseq1[tmp_i] ] += 1
             nfreq1_called[tmp_i][ nseq1[tmp_i] ] += 1
         for tmp_i in range(0,read2_len):
-            nfreq2_raw[tmp_i][ nseq2[tmp_i] ] += 1
             nfreq2_called[tmp_i][ nseq2[tmp_i] ] += 1
         
-        h1_tokens = h_nseq1.split()
-        if( len(h1_tokens) > 1 ):
-            h_nseq1 = '@%s'%h1_tokens[1]
-        h2_tokens = h_nseq2.split()
-        if( len(h2_tokens) > 1 ):
-            h_nseq2 = '@%s'%h2_tokens[1]
+        #h1_tokens = h_nseq1.split()
+        #if( len(h1_tokens) > 1 ):
+        #    h_nseq1 = '@%s'%h1_tokens[1]
+        #h2_tokens = h_nseq2.split()
+        #if( len(h2_tokens) > 1 ):
+        #    h_nseq2 = '@%s'%h2_tokens[1]
 
         f_out.write('%s\n%s\n+\n%s\n'%(h_nseq1,nseq1,qseq1))
         f_out.write('%s\n%s\n+\n%s\n'%(h_nseq2,nseq2,qseq2))
