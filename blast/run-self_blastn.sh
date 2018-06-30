198e8d407d01908b2cf92d608689c14c18b9243f
Taejoon Kwon <tkwon@unist.ac.kr>
2018-06-30 01:53:04 +0000

    Revised script to run self BLASTN

--- blast/run-self_blastn.sh
+++ blast/run-self_blastn.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
-#$ -V
-#$ -cwd
-#$ -j y
-#$ -o $JOB_NAME.o$JOB_ID
-#$ -pe 1way 12
-#$ -q normal
-#$ -l h_rt=24:00:00
-#$ -M $EMAIL
-#$ -m be
-#$ -P hpc
-set -x
-#$ -N mbS.ENGPUs
+
+NUM_THREADS=4
 
 MKDB="$HOME/git/NuevoTx/blast/makeblastdb"
-BLASTN="$HOME/git/NuevoTx/blast/blastn -task megablast -evalue 1e-4 -num_threads 12 -dust yes "
-NRSEQ="$HOME/git/HTseq-toolbox/oTx/make-NR_nseq.py"
+BLASTN="$HOME/git/NuevoTx/blast/blastn -task megablast -evalue 1e-4 -dust yes -max_target_seqs 10"
+NRSEQ="$HOME/git/NuevoTx/combine/make-NR_nseq.py"
 
-FA="foobar.combined_nTx.fa"
 
-DB=${FA/.fa/_NR}
-OUT=$DB'.self.mb+_tbl'
+for FA in $(ls *.combined_nTx.fa)
+do
+  NR_FA=${FA/.fa/_NR.fa}
+  if [ ! -e $NR_FA ]; then
+    echo "Make $NR_FA"
+    $NRSEQ $FA
+  else
+    echo "$NR_FA exists."
+  fi
 
-$NRSEQ $FA
-NR_FA=${FA/.fa/.NR_fa}
+  DB=${NR_FA/.fa/}
+  OUT=$DB'.self.mb+_tbl'
+  
+  if [ ! -e $DB".nin" ]; then
+    echo "Make $DB"
+    $MKDB -dbtype nucl -in $NR_FA -out $DB
+  fi
 
-$MKDB -dbtype nucl -in $NR_FA -out $DB
-time $BLASTN -db $DB -query $NR_FA -out $OUT -outfmt "7 qseqid sseqid pident length mismatch gapopen qlen qstart qend slen sstart send evalue bitscore"
-$BEST $OUT
+  if [ ! -e $OUT ]; then
+    time $BLASTN -db $DB -query $NR_FA -out $OUT -num_threads $NUM_THREADS -outfmt "7 qseqid sseqid pident length mismatch gapopen qlen qstart qend slen sstart send evalue bitscore"
+  fi
+done
