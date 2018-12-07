222e1c2a2b384316a1b955a36f6f99cc49b7ef58
Taejoon Kwon <tkwon@unist.ac.kr>
2018-12-07 10:43:01 +0900

    Revised version of gene_name_bits

--- MODtree/blast_tbl-to-gene_name_bits.py
+++ MODtree/blast_tbl-to-gene_name_bits.py
@@ -18,12 +18,22 @@ for line in f_tbl:
     t_name = tokens[1].split('|')[2].upper()
     tmp_bits = float(tokens[-1])
 
-    if q_name == t_name:
+    if q_name == t_name and tokens[0] != tokens[1]:
         if q_name not in name_bits:
-            name_bits[q_name] = []
-        name_bits[q_name].append(tmp_bits)
+            name_bits[q_name] = dict()
+
+        pair_id = '%s-%s' % (tokens[0], tokens[1])
+        if pair_id not in name_bits[q_name]:
+            name_bits[q_name][pair_id] = tmp_bits
+        elif tmp_bits > name_bits[q_name][pair_id]:
+            name_bits[q_name][pair_id] = tmp_bits
 f_tbl.close()
 
 for tmp_name in sorted(name_bits.keys()):
-    tmp_bits_list = sorted(name_bits[tmp_name])
-    print("%s\t%.1f\t%.1f" % (tmp_name, tmp_bits_list[0], tmp_bits_list[-1]))
+    tmp_pair_list = sorted(name_bits[tmp_name].keys(), key=name_bits[tmp_name].get)
+    min_bits_pair = tmp_pair_list[0]
+    min_bits = name_bits[tmp_name][min_bits_pair]
+    max_bits_pair = tmp_pair_list[-1]
+    max_bits = name_bits[tmp_name][max_bits_pair]
+    print("%s\t%.1f\t%s\t%.1f\t%s" %
+          (tmp_name, max_bits, max_bits_pair, min_bits, min_bits_pair))
