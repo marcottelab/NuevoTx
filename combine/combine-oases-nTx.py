c27f07809eec128db0042bd5a76617ec8f21246c
Taejoon Kwon <taejoon.kwon@gmail.com>
2018-12-26 09:28:16 +0900

    Rename velvet/oases related scripts; Flake8 polish

---
+++ combine/combine-oases-nTx.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+import os
+import sys
+import gzip
+
+prefix = sys.argv[1]
+filename_out = '%s.combined_nTx.fa' % (prefix)
+
+seq_list = dict()
+for filename in os.listdir('.'):
+    if not filename.startswith(prefix):
+        continue
+
+    if not filename.endswith('.nTx.fa.gz'):
+        continue
+
+    if filename == filename_out:
+        continue
+
+    sample_name = filename.replace('.nTx.fa', '')
+
+    f = open(filename, 'r')
+    if filename.endswith('.gz'):
+        sample_name = sample_name.replace('.gz', '')
+        f = gzip.open(filename, 'rt')
+
+    sys.stderr.write('Read %s (%s) -> %s\n' %
+                     (filename, sample_name, filename_out))
+
+    seq_h = ''
+    for line in f:
+        if line.startswith('>'):
+            seq_h = line.strip().replace('_Transcript', '')
+            seq_h = seq_h.replace('Locus', sample_name)
+            seq_h = seq_h.replace('_Confidence', '')
+            seq_h = seq_h.replace('_Length', '')
+            seq_list[seq_h] = []
+        else:
+            seq_list[seq_h].append(line.strip())
+    f.close()
+
+count_total = 0
+multi_seq = dict()
+f_out = open(filename_out, 'w')
+for tmp_h in seq_list.keys():
+    count_total += 1
+    tmp_seq = ''.join(seq_list[tmp_h])
+
+    if tmp_seq not in multi_seq:
+        multi_seq[tmp_seq] = 0
+    multi_seq[tmp_seq] += 1
+
+    f_out.write('%s\n%s\n' % (tmp_h, tmp_seq))
+f_out.close()
