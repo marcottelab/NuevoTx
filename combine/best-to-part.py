8b5cb4b2940a5e3337fcd000f86a0589a18d211a
Taejoon Kwon <tkwon@unist.ac.kr>
2019-03-01 22:32:23 +0900

    Compatible to flake8

--- combine/best-to-part.py
+++ combine/best-to-part.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
-import os 
 import sys
 
 filename_best = sys.argv[1]
-filename_part = filename_best.replace('_best','')+'_part'
+filename_part = filename_best.replace('_best', '')+'_part'
 
 max_gap_opens = 0
 min_align_ratio_pct = 99
 
-f_part = open(filename_part,'w')
-f_best = open(filename_best,'r')
+f_part = open(filename_part, 'w')
+f_best = open(filename_best, 'r')
 for line in f_best:
     if line.startswith('#'):
         continue
@@ -23,10 +22,11 @@ for line in f_best:
     mismatches = int(tokens[5])
     gap_opens = int(tokens[6])
     bitscore = float(tokens[7])
-    
-    align_ratio = (align_len - mismatches)*100.0 / q_len
-    if gap_opens <= max_gap_opens and align_ratio >= min_align_ratio_pct and q_len <= t_len:
-        f_part.write("%s\n"%(line.strip()))
+
+    align_ratio = (align_len - mismatches) * 100.0 / q_len
+    if gap_opens <= max_gap_opens and q_len <= t_len \
+            and align_ratio >= min_align_ratio_pct:
+        f_part.write("%s\n" % (line.strip()))
 
 f_best.close()
 f_part.close()
