87eeecf893646e5b22689a10bc6ae5c50d77fc73
Taejoon Kwon <tkwon@unist.ac.kr>
2019-10-12 10:29:19 +0900

    A script to run genome-guided Trinity

---
+++ denovo/run-STAR+trinity.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+  
+for BAM in $(ls ../STAR/*.sorted.bam)
+do
+  OUT=$(basename $BAM)
+  OUT=${OUT/.STAR.sorted.bam/}".trinity"
+
+  if [ ! -e $OUT ]; then
+        echo "WORK "$OUT
+        Trinity --genome_guided_bam $BAM  --output $OUT \
+         --genome_guided_max_intron 10000 --max_memory 60G --CPU 14
+  else
+          echo "Skip "$OUT
+  fi
+done
