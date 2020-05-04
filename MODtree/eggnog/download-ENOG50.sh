18f7c30a9410ed22f633907fe934cd49660087d6
Taejoon Kwon <tkwon@unist.ac.kr>
2020-05-04 11:47:28 +0900

    Minor improvement.

--- MODtree/eggnog/download-ENOG50.sh
+++ MODtree/eggnog/download-ENOG50.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
-curl -O http://eggnog5.embl.de/download/eggnog_5.0/e5.sequence_aliases.tsv
-curl -O http://eggnog5.embl.de/download/eggnog_5.0/e5.og_annotations.tsv
-curl -O http://eggnog5.embl.de/download/eggnog_5.0/e5.taxid_info.tsv
+URL_BASE="http://eggnog5.embl.de/download/eggnog_5.0"
+for FILE in e5.sequence_aliases.tsv e5.og_annotations.tsv e5.taxid_info.tsv
+do
+  echo $URL_BASE/$FILE
+  curl -O $URL_BASE/$FILE
+done
 
 # 33208_Metazoa
-
 TAX_ID=33208
-URL="http://eggnog5.embl.de/download/eggnog_5.0/per_tax_level"
 
-for FILE in _members.tsv.gz _annotations.tsv.gz _raw_algs.tar _trimmed_algs.tar _trees.tsv.gz _hmms.tar
+
+URL_TAX="http://eggnog5.embl.de/download/eggnog_5.0/per_tax_level"
+
+# For all files
+# for FILE in _members.tsv.gz _annotations.tsv.gz _raw_algs.tar _trimmed_algs.tar _trees.tsv.gz _hmms.tar
+
+# For files only required for MODtree
+for FILE in _members.tsv.gz _annotations.tsv.gz _raw_algs.tar
 do
-  echo $URL/$TAX_ID/$TAX_ID$FILE
-  curl -O $URL/$TAX_ID/$TAX_ID$FILE
+  echo $URL_TAX/$TAX_ID/$TAX_ID$FILE
+  curl -O $URL_TAX/$TAX_ID/$TAX_ID$FILE
 done
 
 #33208/33208_members.tsv.gz
