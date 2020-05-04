48a23cc761330e7279b67dfa14e06fe0e46ff694
Taejoon Kwon <tkwon@unist.ac.kr>
2020-05-04 11:04:11 +0900

    Scripts to download GOA and UniProt References.

---
+++ MODtree/download-GOA.sh
@@ -0,0 +1,15 @@
+curl -O ftp://ftp.ebi.ac.uk/pub/databases/GO/goa/proteomes/4099836.X_laevis_African_clawed_frog.goa
+curl -O ftp://ftp.ebi.ac.uk/pub/databases/GO/goa/proteomes/353236.A_mexicanus.goa
+curl -O ftp://ftp.ebi.ac.uk/pub/databases/GO/goa/proteomes/221256.X_tropicalis.goa
+curl -O ftp://ftp.ebi.ac.uk/pub/databases/GO/goa/proteomes/71242.S_cerevisiae_ATCC_204508.goa
+curl -O ftp://ftp.ebi.ac.uk/pub/databases/GO/goa/proteomes/17.D_melanogaster.goa
+curl -O ftp://ftp.ebi.ac.uk/pub/databases/GO/goa/proteomes/4229144.C_milii.goa
+curl -O ftp://ftp.ebi.ac.uk/pub/databases/GO/goa/proteomes/59.M_musculus.goa
+curl -O ftp://ftp.ebi.ac.uk/pub/databases/GO/goa/proteomes/20721.D_rerio.goa
+curl -O ftp://ftp.ebi.ac.uk/pub/databases/GO/goa/proteomes/25.H_sapiens.goa
+curl -O ftp://ftp.ebi.ac.uk/pub/databases/GO/goa/proteomes/122.R_norvegicus.goa
+curl -O ftp://ftp.ebi.ac.uk/pub/databases/GO/goa/proteomes/271521.T_nigroviridis_1.goa
+curl -O ftp://ftp.ebi.ac.uk/pub/databases/GO/goa/proteomes/271518.L_chalumnae.goa
+curl -O ftp://ftp.ebi.ac.uk/pub/databases/GO/goa/proteomes/21457.G_gallus.goa
+curl -O ftp://ftp.ebi.ac.uk/pub/databases/GO/goa/proteomes/264665.A_carolinensis.goa
+curl -O ftp://ftp.ebi.ac.uk/pub/databases/GO/goa/proteomes/221259.C_intestinalis.goa
--- MODtree/download-uniprot.sh
+++ MODtree/download-uniprot.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-curl -O ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/relnotes.txt
+URL="ftp://ftp.uniprot.org/pub/databases/uniprot/current_release"
+echo "Download ReleaseNotes ..."
+curl -O $URL"/relnotes.txt"
+echo "Download SpeciesList ..."
 curl -O https://www.uniprot.org/docs/speclist
 
 SRC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -9,15 +12,21 @@ REF_LIST=$SRC_DIR"/MODtree_species.txt"
 if [ ! -e $REF_LIST ]; then
   echo "Not available: Uniprot Ref Proteome List"
   echo "Current setting: "$REF_LIST
-
-else
-
-  for UP_ID in $(grep -v ^# $REF_LIST | awk '{print $1"_"$2}')
-  do
-    echo $UP_ID
-    curl -O ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/reference_proteomes/Eukaryota/$UP_ID".fasta.gz"
-    curl -O ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/reference_proteomes/Eukaryota/$UP_ID"_DNA.fasta.gz"
-    curl -O ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/reference_proteomes/Eukaryota/$UP_ID".gene2acc.gz"
-    curl -O ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/reference_proteomes/Eukaryota/$UP_ID".idmapping.gz"
-  done
+  exit
 fi
+
+for UP_ID in $(grep -v ^# $REF_LIST | awk '{print $1"_"$2}')
+do
+  echo $UP_ID
+  #for SUFFIX in ".fasta.gz" "_DNA.fasta.gz" ".gene2acc.gz" ".idmapping.gz"
+  for SUFFIX in ".fasta.gz"
+  do
+    TARGET_FILE=$UP_ID$SUFFIX
+    if [ ! -e $TARGET_FILE ]; then
+      echo "Download "$TARGET_FILE
+      curl -O ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/reference_proteomes/Eukaryota/$TARGET_FILE
+    else
+      echo $TARGET_FILE" exists. Skip."
+    fi
+  done
+done
