2326065d9b9595be15b9e2b85f36ac55950f8268
Taejoon Kwon <taejoon.kwon@gmail.com>
2020-08-25 01:44:55 +0900

    Scripts for salmon filter.

--- salmon/run-salmon_idx.sh
+++ salmon/run-salmon_idx.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 NUM_THREADS=4
 
+# https://github.com/COMBINE-lab/salmon/releases/download/v1.0.0/salmon-1.0.0_linux_x86_64.tar.gz
+SALMON_EXE="$HOME/src/salmon/salmon-1.1.0/bin/salmon"
+
 for FA in $(ls *_NoPart_nTx.fa)
 do
   OUT=${FA/_NoPart_nTx.fa/}".salmon_idx"
   if [ ! -e $OUT ]; then
     echo "Create $OUT"
-    salmon index -p $NUM_THREADS -t $FA -i $OUT
+    $SALMON_EXE index -p $NUM_THREADS -t $FA -i $OUT
   fi
 done
