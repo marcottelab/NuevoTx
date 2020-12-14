ac2046bad671803e20f310c364529bc27f2a0f4c
Taejoon Kwon <tkwon@unist.ac.kr>
2020-12-15 01:39:14 +0900

    minor polishing for run-STAR+trinity.sh

--- denovo/run-STAR+trinity.sh
+++ denovo/run-STAR+trinity.sh
@@ -6,10 +6,10 @@ do
   OUT=${OUT/.STAR.sorted.bam/}".trinity"
 
   if [ ! -e $OUT ]; then
-        echo "WORK "$OUT
-        Trinity --genome_guided_bam $BAM  --output $OUT \
-         --genome_guided_max_intron 10000 --max_memory 60G --CPU 14
+    echo "WORK "$OUT
+    Trinity --genome_guided_bam $BAM  --output $OUT \
+            --genome_guided_max_intron 10000 --max_memory 60G --CPU 14
   else
-          echo "Skip "$OUT
+    echo "Skip "$OUT
   fi
 done
