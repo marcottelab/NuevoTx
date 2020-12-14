7026814f299102526e83927ef7daa60c614f1c64
Taejoon Kwon <tkwon@unist.ac.kr>
2020-12-15 01:40:19 +0900

    minor polishing: run-rnaSPAdes.sh

--- denovo/run-rnaSPAdes.sh
+++ denovo/run-rnaSPAdes.sh
@@ -8,14 +8,15 @@
 # Ref. http://cab.spbu.ru/software/spades/
 
 NUM_THREADS=8
-DIR_FQ="../fastq.TKLab202010/"
+DIR_FQ="../fastq/"
 
 # Run trim+flash first.
-for FQ_e in $(ls $DIR_FQ/*Testis*.extendedFrags.fastq)
+for FQ_e in $(ls $DIR_FQ/*.extendedFrags.fastq)
 do
   FQ_nc=${FQ_e/.extendedFrags.fastq/}".notCombined.fastq"
   OUT=$(basename $FQ_e)
   OUT=${OUT/.extendedFrags.fastq/}".spades"
+
   if [ ! -e $OUT ]; then
     echo "Make "$OUT
     rnaspades.py --merged $FQ_e --12 $FQ_nc -t $NUM_THREADS -o $OUT
