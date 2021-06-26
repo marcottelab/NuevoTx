1387628358afea77b32918724367d319de88d9c4
Taejoon Kwon <taejoon.kwon@gmail.com>
2021-06-26 17:50:05 +0900

    refine the script to run kallisto

--- quant/run-kallisto_nTx.sh
+++ quant/run-kallisto_nTx.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# install kallisto first via conda
+# $ conda install -c bioconda kallisto
+
 NUM_THREADS=8
 DIR_FQ="../fastq/"
 
-for FA in $(ls *_nTx_NR.fa)
+for FA in $(ls *.fa)
 do
   IDX=${FA/.fa/}".kallisto_idx"
   echo "Make "$IDX
@@ -12,9 +15,17 @@ do
   OUT=${IDX/.kallisto_idx/}".kallisto_quant"
 
   SAMPLE_NAME=$(echo $FA | awk -F"." '{print $1}')
+
+  # for trimmed reads
   FQ1=$SAMPLE_NAME"_trim_1P.gz"
   FQ1=$DIR_FQ"/"$FQ1
   FQ2=${FQ1/_1P/_2P}
+
+  # for raw reads
+  # FQ1=$SAMPLE_NAME"_R1.raw.fastq.gz"
+  # FQ1=$DIR_FQ"/"$FQ1
+  # FQ2=${FQ1/_R1/_R2}
+
   echo "FASTQ:" $FQ1 $FQ2
 
   if [ -d $OUT ]; then
