9893bf5f940725b39e97cecb69dc31aa0f82321a
Taejoon Kwon <taejoon.kwon@gmail.com>
2021-07-20 15:35:49 +0900

    register FLASH script

--- denovo/02-run-flash.sh
+++ denovo/02-run-flash.sh
@@ -3,11 +3,10 @@
 NUM_THREADS=4
 MAX_OVERLAP=90
 
-for R1 in $(ls *_trim_1P)
+for R1 in $(ls *_trim_1P.gz)
 do
   R2=${R1/_1P/_2P}
-  #OUT_NAME=$(echo $R1 | awk -F ".R1" '{print $1}')
-  OUT_NAME=${R1/_trim_1P/}
+  OUT_NAME=${R1/_trim_1P.gz/}
   echo $R1 $R2 $OUT_NAME
   flash $R1 $R2 -o $OUT_NAME -t $NUM_THREADS -M $MAX_OVERLAP --interleaved-output
 done
