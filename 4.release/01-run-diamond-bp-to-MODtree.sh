1a01101d61056d0b2d62a4eaf5934606812de13d
Taejoon Kwon <taejoon.kwon@gmail.com>
2021-08-08 17:57:22 +0900

    read MODtree info from the conf file

--- 4.release/01-run-diamond-bp-to-MODtree.sh
+++ 4.release/01-run-diamond-bp-to-MODtree.sh
@@ -9,7 +9,11 @@ source $MODTREE_CONF
 echo "MODtree Name:" $MODTREE_NAME 
 echo "MODtree Path:" $MODTREE_PATH
 
+DB=$MODTREE_PATH
+DB_NAME=$MODTREE_NAME
+
 FA=$1
+
 OUT_TBL=${FA/.fa/}"."$DB_NAME".dmnd_bp_tbl6"
 if [ ! -e $OUT_TBL ]; then
       diamond blastp -b4 --threads $NUM_THREADS --max-target-seqs 10 --query $FA --db $DB --outfmt 6 --out $OUT_TBL
